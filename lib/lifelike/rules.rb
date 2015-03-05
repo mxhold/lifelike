aa98f715d1e59ef51bda4408157785fce2133d36
Max Holder <mxhold@gmail.com>
2015-03-04 21:24:14 -0500

    Extract RuleString

--- lib/lifelike/rules.rb
+++ lib/lifelike/rules.rb
@@ -1,35 +1,37 @@
 module Lifelike
   class Rules
-    # See: http://www.conwaylife.com/wiki/Rule#Rules
-    # Example: B3/S23
-    def initialize(rule_string)
-      @rule_string = rule_string
+    def initialize(string)
+      @rule_string = RuleString.new(string)
     end
 
     def survives?(alive_neighbor_count)
-      alive_neighbors_to_survive.include?(alive_neighbor_count)
+      @rule_string.alive_neighbors_to_survive.include?(alive_neighbor_count)
     end
 
     def born?(alive_neighbor_count)
-      alive_neighbors_to_be_born.include?(alive_neighbor_count)
+      @rule_string.alive_neighbors_to_be_born.include?(alive_neighbor_count)
+    end
+  end
+
+  class RuleString
+    # See: http://www.conwaylife.com/wiki/Rule#Rules
+    # Example: B3/S23
+    def initialize(string)
+      @string = string
+    end
+
+    def alive_neighbors_to_be_born
+      numbers_after('B')
+    end
+
+    def alive_neighbors_to_survive
+      numbers_after('S')
     end
 
     private
 
-    def alive_neighbors_to_be_born
-      parse_rule_part('B')
-    end
-
-    def alive_neighbors_to_survive
-      parse_rule_part('S')
-    end
-
-    def parse_rule_part(letter)
-      numbers_after_letter(@rule_string, letter)
-    end
-
-    def numbers_after_letter(string, letter)
-      string[/#{letter}(\d*)/, 1].split('').map(&:to_i)
+    def numbers_after(letter)
+      @string[/#{letter}(\d*)/, 1].split('').map(&:to_i)
     end
   end
 end
