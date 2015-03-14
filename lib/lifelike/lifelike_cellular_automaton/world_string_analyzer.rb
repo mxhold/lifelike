be74b8fdbc51b8ca67e3e5da9a48eebbcd9e2e57
Max Holder <mxhold@gmail.com>
2015-03-14 19:06:54 -0400

    Refactor WorldStringAnalyzer
    This removes the complicated and no longer necessary frequency
    calculation part.
    
    I’m still not a fan of the case statements here but this is an
    improvement at least.

--- lib/lifelike/lifelike_cellular_automaton/world_string_analyzer.rb
+++ lib/lifelike/lifelike_cellular_automaton/world_string_analyzer.rb
@@ -8,47 +8,59 @@ module Lifelike
       end
 
       def dead_char
-        @dead_char ||= two_most_frequent_valid_chars_by_aliveness.fetch(0)
+        case valid_chars.size
+        when 0
+          raise InsufficientValidCharacterError.new(allowed_chars_by_aliveness)
+        when 1
+          deadlike_char || @fallback_dead_char
+        else
+          least_alive_valid_char
+        end
       end
 
       def alive_char
-        @alive_char ||= two_most_frequent_valid_chars_by_aliveness.fetch(1)
+        case valid_chars.size
+        when 0
+          raise InsufficientValidCharacterError.new(allowed_chars_by_aliveness)
+        when 1
+          lifelike_char || @fallback_alive_char
+        else
+          most_alive_valid_char
+        end
       end
 
       private
 
-      def two_most_frequent_valid_chars_by_aliveness
-        case valid_chars_by_frequency.size
-        when 0
-          raise InsufficientValidCharacterError.new(valid_possible_chars_by_aliveness)
-        when 1
-          if deadlike.include?(valid_chars.first)
-            [valid_chars.first, @fallback_alive_char]
-          else
-            [@fallback_dead_char, valid_chars.first]
-          end
-        else
-          valid_chars_by_frequency.take(2).sort_by { |c| aliveness(c) }
-        end
+      def deadlike_char
+        valid_char if deadlike.include?(valid_char)
       end
 
-      def valid_chars_by_frequency
-        valid_char_frequencies.sort_by { |c, f| -f }.to_h.keys
+      def lifelike_char
+        valid_char if lifelike.include?(valid_char)
       end
 
-      def valid_char_frequencies
-        valid_chars.reduce({}) do |frequencies, char|
-          frequencies[char] = frequencies.fetch(char, 0) + 1
-          frequencies
-        end
+      def valid_char
+        valid_chars.first
+      end
+
+      def least_alive_valid_char
+        valid_chars_by_aliveness.first
+      end
+
+      def most_alive_valid_char
+        valid_chars_by_aliveness.last
+      end
+
+      def valid_chars_by_aliveness
+        valid_chars.take(2).sort_by { |c| aliveness(c) }
       end
 
       def valid_chars
-        @world_string.chars.select { |c| valid_possible_chars_by_aliveness.include?(c) }
+        @world_string.chars.uniq.select { |c| allowed_chars_by_aliveness.include?(c) }
       end
 
       # Roughly in order from most dead-like to most alive-like
-      def valid_possible_chars_by_aliveness
+      def allowed_chars_by_aliveness
         deadlike + lifelike
       end
 
@@ -61,7 +73,7 @@ module Lifelike
       end
 
       def aliveness(char)
-        valid_possible_chars_by_aliveness.index(char)
+        allowed_chars_by_aliveness.index(char)
       end
     end
   end
